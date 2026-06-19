8652c3728b387f7a4a66ec59354fc16c6212bf9a
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2026-06-19 19:27:36 +0100

    new api

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000/api" : "https://chemicalbusinessreports.onrender.com/api";
+const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000/api" : "https://chemicalbusinessreports-f078.onrender.com";
 
 function getAuthHeaders() {
     const token = typeof window !== 'undefined' ? localStorage.getItem('adminToken') : null;
