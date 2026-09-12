394e2adaf9ce7575e72c2a9f56de683c082605fa
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2026-09-12 07:00:37 +0100

    new chemical business mart

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -31,7 +31,7 @@ const allMobileLinks = [
     { name: "Home", href: "/" },
     { name: "About Us", href: "/about" },
     { name: "News Roundup", href: "/posts/news-roundup" },
-    { name: "Chemical Mart", href: "/posts/chemical-mart" },
+    { name: "Chemical Business Mart", href: "/posts/chemical-mart" },
     { name: "ChemTalk", href: "/chemtalk" },
     { name: "Awards & Partners", href: "/awards" },
     { name: "Research & Reports", href: "/posts/research-reports" },
